1ce6710305338b8e3c1ddf9826f7bea697cc53e6
prom12 <51293420+Prom12@users.noreply.github.com>
2022-04-28 13:57:29 +0000

    Update employee data to allow insert

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import oracledb from "oracledb";
 import cors from "cors";
+import farmhash from "farmhash";
 
 const app = express();
 
@@ -64,7 +65,7 @@ async function selectEmployeesById(req, res, id) {
     });
     // run query to get employee with employee_id
     result = await connection.execute(
-      `SELECT * FROM employees where employee_id=:id`,
+      `SELECT * FROM EMPLOYEE where EMPLOYEEID=:id`,
       [id]
     );
   } catch (err) {
@@ -117,4 +118,121 @@ app.get("/employee", function (req, res) {
 
 app.get("/", (req, res) => {
   res.send("Server is running");
+});
+
+// Insert function for adding to any table
+async function insertEmployee(req, res, tableName, tableData, id, payload) {
+  try {
+    connection = await oracledb.getConnection({
+      user: user,
+      password: password,
+      connectString: connectionString,
+    });
+
+    // tableData
+    // run query to get employee with employee_id
+    // result = await connection.execute(
+    //   `INSERT INTO EMPLOYEE VALUES (${id}, ${tableData})`
+    // );
+
+    result = await connection.execute(
+      `INSERT INTO EMPLOYEE VALUES (:0, :1,:2, :3, :4,:5, :6, :7, :8, :9,:10, :11, :12, :13, :14)`,
+      [
+        id,
+        payload.title,
+        payload.firstName,
+        payload.middleName,
+        payload.lastName,
+        payload.address,
+        payload.workTelNo,
+        payload.homeTelNo,
+        payload.employerAddress,
+        payload.socialSecurity,
+        new Date(),
+        payload.position,
+        payload.sex,
+        payload.salary,
+        new Date(),
+      ],
+      { autoCommit: true },
+      function (err, result) {
+        if (err) {
+          return res.send(err);
+        } else {
+          return res.send("Data Successfully Saved");
+        }
+      }
+    );
+  } catch (err) {
+    //send error message
+    return res.send(err.message);
+  } finally {
+    if (connection) {
+      try {
+        // Always close connections
+        await connection.close();
+        console.log("close connection success");
+      } catch (err) {
+        return console.error(err.message);
+      }
+    }
+  }
+}
+
+// Post connection URLS, for adding to the database
+
+app.post("/newEmployee", function (req, res) {
+  console.log(req.body);
+  // console.log(res);
+  try {
+    var { payload } = [];
+    payload = req.body;
+
+    const employeeNo = farmhash.hash64(
+      new Buffer.from(
+        payload.title +
+          payload.firstName +
+          payload.middleName +
+          payload.socialSecurity +
+          payload.sex
+      )
+    );
+    console.log("employeeNo");
+    console.log(employeeNo);
+
+    let tableName = "EMPLOYEE";
+    let tableData = payload;
+    let id = employeeNo;
+
+    insertEmployee(req, res, tableName, tableData, id, payload);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send();
+  }
+});
+
+app.post("/newCustomer", async function (req, res) {
+  try {
+    var { payload } = [];
+    payload = req.body;
+
+    const customerNo = farmhash.hash64(
+      new Buffer.from(
+        payload.zipCode +
+          payload.name +
+          payload.state +
+          payload.telNumber +
+          payload.maritalStatus
+      )
+    );
+
+    let tableName = "CUSTOMER";
+    let tableData = payload;
+    let id = customerNo;
+
+    insertEmployee(req, res, tableName, tableData, id);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send();
+  }
 });
